7af741132adfea2d4ae10762239bb69323c74747
RobertoZeppilli <robzep171@gmail.com>
2021-05-17 15:31:10 +0200

    Vue Hello (Testo bottone dinamico)

--- js/script.js
+++ js/script.js
@@ -10,19 +10,22 @@ let app = new Vue(
         el: "#root",
         data: {
             testo: "Paesaggio all'alba",
+            testoBottone: "Vedi il tramonto",
             immagine: 'img/paesaggio1.jpg',
             colore: "light"
         },
         methods: {
             cambiaImmagine: function () {
-                if (this.immagine == 'img/paesaggio1.jpg' && this.testo == "Paesaggio all'alba") {
+                if (this.immagine == 'img/paesaggio1.jpg') {
                     this.immagine = 'img/paesaggio2.jpg';
                     this.testo = "Paesaggio al tramonto";
-                    this.colore = "dark"
+                    this.colore = "dark";
+                    this.testoBottone = "Torna all'alba"
                 } else {
                     this.immagine = 'img/paesaggio1.jpg';
                     this.testo = "Paesaggio all'alba";
-                    this.colore = "light"
+                    this.colore = "light";
+                    this.testoBottone = "Vedi il tramonto"
                 }
             }
         }
